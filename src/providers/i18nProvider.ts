81311f002a37e3fb594511541e0aecdd41f8581f
Mauricio Arellano <mac_corp@hotmail.com>
2025-11-20 12:02:35 -0600

    Using The Browser Locale.

--- src/providers/i18nProvider.ts
+++ src/providers/i18nProvider.ts
@@ -1,3 +1,4 @@
+import { resolveBrowserLocale } from 'react-admin';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import englishMessages from 'ra-language-english';
 import spanishMessages from "@blackbox-vision/ra-language-spanish";
@@ -5,7 +6,8 @@ import spanishMessages from "@blackbox-vision/ra-language-spanish";
 const translations = { en: englishMessages, sp: spanishMessages };
 
 export const i18nProvider = polyglotI18nProvider(
-    locale => translations[locale],
-    'sp', // default locale
-    [{ locale: 'en', name: 'English' }, { locale: 'sp', name: 'Español' }],
+    locale => translations[locale] ? translations[locale] : translations.sp,
+    resolveBrowserLocale('sp'), //'sp', // default locale
+    [{ locale: 'en', name: 'English' }, 
+     { locale: 'sp', name: 'Español' }],
 );
